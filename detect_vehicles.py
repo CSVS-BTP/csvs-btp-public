6b9f4d356d9c82d7278aafe27d88147fc852fa9f
vishak3 <vishak@nichesoft.net>
2024-09-10 22:40:43 +0530

    Adding comments

--- detect_vehicles.py
+++ detect_vehicles.py
@@ -21,7 +21,7 @@ else:
 model = YOLO("best.pt")
 
 # Load a pre-trained Inception_v3 model
-feature_extractor = inception_v3(weights='Inception_V3_Weights.IMAGENET1K_V1')
+feature_extractor = inception_v3(weights="Inception_V3_Weights.IMAGENET1K_V1")
 # Remove the final classification layer
 feature_extractor.classifier = torch.nn.Identity()
 feature_extractor.eval()
@@ -40,71 +40,73 @@ preprocess = transforms.Compose(
 
 # Define vehicle class map and IDs
 vehicle_class_rmap = {
-    0:'Bicycle',
-    1:'Bus',
-    2:'Car',
-    3:'LCV',
-    4:'Three-Wheeler',
-    5:'Truck',
-    6:'Two-Wheeler',
-} 
+    0: "Bicycle",
+    1: "Bus",
+    2: "Car",
+    3: "LCV",
+    4: "Three-Wheeler",
+    5: "Truck",
+    6: "Two-Wheeler",
+}
 # Define turns map for each cam_id
 cam_id_turns_map = {
-    'Stn_HD_1' : ['BC','BE','DE','DA','FA','FC'],
-    'Sty_Wll_Ldge_FIX_3' : ['BA','AB'],
-    'SBI_Bnk_JN_FIX_1' : ['AB','AC','CA','CB','BA','BC'],
-    'SBI_Bnk_JN_FIX_3' : ['AB','BA'],
-    '18th_Crs_BsStp_JN_FIX_2' : ['AB','BA'],
-    '18th_Crs_Bus_Stop_FIX_2' : ['AB','AD','EB','ED','CD'],
-    'Ayyappa_Temple_FIX_1' : ['AB','BA'],
-    'Devasandra_Sgnl_JN_FIX_1' : ['BA','AB'],
-    'Devasandra_Sgnl_JN_FIX_3' : ['AB','AD','DA','DB','CD','CA'],
-    'Mattikere_JN_FIX_1' : ['AB','AD','DA','DB','CD','CA'],
-    'Mattikere_JN_FIX_2' : ['DA','DC','CA','BC','BD'],
-    'Mattikere_JN_FIX_3' : ['AB','BA'],
-    'Mattikere_JN_HD_1' : ['AC','BA','BD','CA','CD'],
-    'HP_Ptrl_Bnk_BEL_Rd_FIX_2' : ['AB','BA'],
-    'Kuvempu_Circle_FIX_1' : ['BA'],
-    'Kuvempu_Circle_FIX_2' : ['BA'],
-    'MS_Ramaiah_JN_FIX_1' : ['AC','AB'],
-    'MS_Ramaiah_JN_FIX_2' : ['HA','HC','HE','FG','FA','FC','DA','DG','DE','BC','BE','BG'],
-    'Ramaiah_BsStp_JN_FIX_1' : ['BA','AB'],
-    'Ramaiah_BsStp_JN_FIX_2' : ['AB','BA'],
-    '18th_Crs_BsStp_JN_FIX_1' : [],
-    '18th_Crs_Bus_Stop_FIX_1' : [],
-    'MS_Ramaiah_JN_FIX_3' : [],
-    'Udayagiri_JN_FIX_2' : [],
-    'Dari_Anjaneya_Temple': ['CD','BC','BD','AC','AD'],
-    'Nanjudi_House': ['CA','CB','BC','AC'],
-    'Buddha_Vihara_Temple': ['AB','BA'],
-    'Sundaranagar_Entrance': ['AB','BA'],
-    'ISRO_Junction': ['AC','AB','BD','BC'],
-    '80ft_Road': ['BA','AB'],
+    "Stn_HD_1": ["BC", "BE", "DE", "DA", "FA", "FC"],
+    "Sty_Wll_Ldge_FIX_3": ["BA", "AB"],
+    "SBI_Bnk_JN_FIX_1": ["AB", "AC", "CA", "CB", "BA", "BC"],
+    "SBI_Bnk_JN_FIX_3": ["AB", "BA"],
+    "18th_Crs_BsStp_JN_FIX_2": ["AB", "BA"],
+    "18th_Crs_Bus_Stop_FIX_2": ["AB", "AD", "EB", "ED", "CD"],
+    "Ayyappa_Temple_FIX_1": ["AB", "BA"],
+    "Devasandra_Sgnl_JN_FIX_1": ["BA", "AB"],
+    "Devasandra_Sgnl_JN_FIX_3": ["AB", "AD", "DA", "DB", "CD", "CA"],
+    "Mattikere_JN_FIX_1": ["AB", "AD", "DA", "DB", "CD", "CA"],
+    "Mattikere_JN_FIX_2": ["DA", "DC", "CA", "BC", "BD"],
+    "Mattikere_JN_FIX_3": ["AB", "BA"],
+    "Mattikere_JN_HD_1": ["AC", "BA", "BD", "CA", "CD"],
+    "HP_Ptrl_Bnk_BEL_Rd_FIX_2": ["AB", "BA"],
+    "Kuvempu_Circle_FIX_1": ["BA"],
+    "Kuvempu_Circle_FIX_2": ["BA"],
+    "MS_Ramaiah_JN_FIX_1": ["AC", "AB"],
+    "MS_Ramaiah_JN_FIX_2": ["HA", "HC", "HE", "FG", "FA", "FC", "DA", "DG", "DE", "BC", "BE", "BG"],
+    "Ramaiah_BsStp_JN_FIX_1": ["BA", "AB"],
+    "Ramaiah_BsStp_JN_FIX_2": ["AB", "BA"],
+    "18th_Crs_BsStp_JN_FIX_1": [],
+    "18th_Crs_Bus_Stop_FIX_1": [],
+    "MS_Ramaiah_JN_FIX_3": [],
+    "Udayagiri_JN_FIX_2": [],
+    "Dari_Anjaneya_Temple": ["CD", "BC", "BD", "AC", "AD"],
+    "Nanjudi_House": ["CA", "CB", "BC", "AC"],
+    "Buddha_Vihara_Temple": ["AB", "BA"],
+    "Sundaranagar_Entrance": ["AB", "BA"],
+    "ISRO_Junction": ["AC", "AB", "BD", "BC"],
+    "80ft_Road": ["BA", "AB"],
 }
+
 
 def detect_vehicles(cam_id, video_file):
 
-    vcam_id = video_file.split('/')[-1].split('.')[0]
-    print(vcam_id)
+    # Check for known cam_ids
+    vcam_id = video_file.split("/")[-1].split(".")[0]
     if vcam_id not in cam_id_turns_map:
-        cam_id_turns_map[vcam_id] = ['AB', 'BA']
+        cam_id_turns_map[vcam_id] = ["AB", "BA"]
     cam_id_turns_map[cam_id] = cam_id_turns_map[vcam_id]
 
-    csv_file=f'{cam_id}.csv'
-    
+    # Save output
+    csv_file = f"{cam_id}.csv"
+
     results = model.track(
         source=video_file,
         device=device,
-        imgsz = 1152,
-        conf = 0.1,
-        iou = 0.5,
+        imgsz=1152,
+        conf=0.1,
+        iou=0.5,
         max_det=100,
-        agnostic_nms = True,
-        vid_stride = 1,
-        stream = True,
-        tracker = "custom_botsort.yaml",
-        persist = True,
-        verbose = False
+        agnostic_nms=True,
+        vid_stride=1,
+        stream=True,
+        tracker="custom_botsort.yaml",
+        persist=True,
+        verbose=False,
     )
 
     ob_id = 0
@@ -130,7 +132,7 @@ def detect_vehicles(cam_id, video_file):
             roi = preprocess(ob_crop).unsqueeze(0).to(device)
             with torch.no_grad():
                 features = feature_extractor(roi).squeeze().cpu()
-                
+
             features_dict = {
                 "fn": fn,
                 "v_id": v_id,
@@ -145,65 +147,64 @@ def detect_vehicles(cam_id, video_file):
             ob_id += 1
 
     idf = pd.DataFrame(instance_dict).T
-    idf['fn'] = idf['fn'].astype(int)
-    idf['cls_id'] = idf['cls_id'].astype(int)
-    idf['v_id'] = idf['v_id'].astype(float)
+    idf["fn"] = idf["fn"].astype(int)
+    idf["cls_id"] = idf["cls_id"].astype(int)
+    idf["v_id"] = idf["v_id"].astype(float)
 
-    tdf_drop = idf.groupby('v_id')['fn'].count().reset_index()
-    vdrop = tdf_drop.loc[tdf_drop['fn'] < 2]['v_id'].values.tolist() # minimum of 2 instances filter
-    idf.drop(idf.loc[idf['v_id'].isin(vdrop + [np.nan])].index, inplace=True)
+    tdf_drop = idf.groupby("v_id")["fn"].count().reset_index()
+    vdrop = tdf_drop.loc[tdf_drop["fn"] < 2][
+        "v_id"
+    ].values.tolist()  # minimum of 2 instances filter
+    idf.drop(idf.loc[idf["v_id"].isin(vdrop + [np.nan])].index, inplace=True)
     idf.reset_index(drop=True, inplace=True)
 
-    vidfx = idf.groupby('v_id')['x'].aggregate(['first','last']).reset_index()
-    vidfy = idf.groupby('v_id')['y'].aggregate(['first','last']).reset_index()
-    vidft = idf.groupby('v_id')['fn'].median().astype(int).reset_index()
-    vdf = pd.merge(vidfx, vidfy, how='inner', on='v_id', suffixes=['_x', '_y'])
-    vdf = pd.merge(vdf, vidft, how='inner', on='v_id')
-    vdf['delta_x'] = vdf['first_x'] - vdf['last_x']
-    vdf['delta_y'] = vdf['first_y'] - vdf['last_y']
-    vids = vdf.loc[(vdf['delta_x'].abs()>5) & (vdf['delta_x'].abs()>5)]['v_id'] # Ignore vehicles which haven't moved at least 5 pixels
+    # Determine turns/direction of movement of each vehicle
+    vidfx = idf.groupby("v_id")["x"].aggregate(["first", "last"]).reset_index()
+    vidfy = idf.groupby("v_id")["y"].aggregate(["first", "last"]).reset_index()
+    vidft = idf.groupby("v_id")["fn"].median().astype(int).reset_index()
+    vdf = pd.merge(vidfx, vidfy, how="inner", on="v_id", suffixes=["_x", "_y"])
+    vdf = pd.merge(vdf, vidft, how="inner", on="v_id")
+    vdf["delta_x"] = vdf["first_x"] - vdf["last_x"]
+    vdf["delta_y"] = vdf["first_y"] - vdf["last_y"]
+    vids = vdf.loc[(vdf["delta_x"].abs() > 5) & (vdf["delta_x"].abs() > 5)]["v_id"]  # Ignore vehicles which haven't moved at least 5 pixels
 
-    vdf = vdf.loc[vdf['v_id'].isin(vids)]
-    idf = idf.loc[idf['v_id'].isin(vids)]
+    vdf = vdf.loc[vdf["v_id"].isin(vids)]
+    idf = idf.loc[idf["v_id"].isin(vids)]
 
-    tvtype_df = idf.groupby(['v_id'])['cls_id'].value_counts().reset_index()
-    idxs = tvtype_df.groupby(['v_id'])['count'].idxmax()
-                
-    vdf['cls_id'] = tvtype_df.loc[idxs]['cls_id'].values
-    vdf['vehicle'] = vdf['cls_id'].map(vehicle_class_rmap)
+    tvtype_df = idf.groupby(["v_id"])["cls_id"].value_counts().reset_index()
+    idxs = tvtype_df.groupby(["v_id"])["count"].idxmax()
 
-    dcols = ['first_x', 'last_x', 'first_y', 'last_y']
-    tvdf = vdf.drop(columns = dcols).merge(idf.drop(columns=['cls_id']), on=['v_id', 'fn'])
-    tvdf.drop(columns=['v_id'], inplace=True)
+    vdf["cls_id"] = tvtype_df.loc[idxs]["cls_id"].values
+    vdf["vehicle"] = vdf["cls_id"].map(vehicle_class_rmap)
+
+    dcols = ["first_x", "last_x", "first_y", "last_y"]
+    tvdf = vdf.drop(columns=dcols).merge(
+        idf.drop(columns=["cls_id"]), on=["v_id", "fn"]
+    )
+    tvdf.drop(columns=["v_id"], inplace=True)
 
     turns_list = cam_id_turns_map[cam_id]
 
-    tvdf.reset_index(names='v_id', inplace=True)
+    tvdf.reset_index(names="v_id", inplace=True)
 
-    tvdf['theta'] = np.arctan2(tvdf['delta_y'].astype(int), tvdf['delta_x'].astype(int))
-    tvdf['cos_theta'] = np.cos(tvdf['theta'])
-    tvdf['sin_theta'] = np.sin(tvdf['theta'])
-    tvdf['angle'] = np.rad2deg(tvdf['theta'])
+    tvdf["theta"] = np.arctan2(tvdf["delta_y"].astype(int), tvdf["delta_x"].astype(int))
+    tvdf["cos_theta"] = np.cos(tvdf["theta"])
+    tvdf["sin_theta"] = np.sin(tvdf["theta"])
+    tvdf["angle"] = np.rad2deg(tvdf["theta"])
 
     agc = AgglomerativeClustering(n_clusters=len(turns_list))
-    tvdf['cluster'] = agc.fit_predict(tvdf[['cos_theta', 'sin_theta']])
-    cluster_centers = tvdf.groupby('cluster')['angle'].median().reset_index()
-    cluster_centers.sort_values(by='angle', key=lambda x: (x - 90) % 360, inplace=True)
+    tvdf["cluster"] = agc.fit_predict(tvdf[["cos_theta", "sin_theta"]])
+    cluster_centers = tvdf.groupby("cluster")["angle"].median().reset_index()
+    cluster_centers.sort_values(by="angle", key=lambda x: (x - 90) % 360, inplace=True)
     cluster_centers.reset_index(drop=True, inplace=True)
-    cluster_centers['Turns'] = turns_list
-    cluster_map = cluster_centers[['cluster','Turns']].set_index('cluster').to_dict()['Turns']
+    cluster_centers["Turns"] = turns_list
+    cluster_map = (
+        cluster_centers[["cluster", "Turns"]].set_index("cluster").to_dict()["Turns"]
+    )
 
-    tvdf['Turning Patterns'] = tvdf['cluster'].map(cluster_map)
+    tvdf["Turning Patterns"] = tvdf["cluster"].map(cluster_map)
 
-    cols = ['fn',
-    'cls_id',
-    'vehicle',
-    'features',
-    'x',
-    'y',
-    'w',
-    'h',
-    'Turning Patterns']
+    cols = ["fn", "cls_id", "vehicle", "features", "x", "y", "w", "h", "Turning Patterns"]
 
-    tvdf['features'] = tvdf['features'].apply(lambda x: x.tolist())
+    tvdf["features"] = tvdf["features"].apply(lambda x: x.tolist())
     tvdf[cols].to_csv(csv_file, index=False)
--- reid_vehicles.py
+++ reid_vehicles.py
@@ -17,8 +17,10 @@ else:
     print("GPU is not available")
     device = torch.device("cpu")
 
-vehicle_list = ['Bicycle', 'Bus', 'Car', 'LCV', 'Three-Wheeler', 'Truck', 'Two-Wheeler']
+vehicle_list = ["Bicycle", "Bus", "Car", "LCV", "Three-Wheeler", "Truck", "Two-Wheeler"]
 
+
+# Permutations of roads between cams
 def pair_buckets_with_reuse(bucket1, bucket2):
     max_length = max(len(bucket1), len(bucket2))
     # Repeat the smaller bucket elements to match the size of the larger bucket
@@ -37,71 +39,95 @@ def pair_buckets_with_reuse(bucket1, bucket2):
 
     return pairings
 
-def reid_vehicles(cam_ids, videos, output_dir = '/app/data/CSVS'):
 
+def reid_vehicles(cam_ids, videos, output_dir="/app/data/CSVS"):
+
+    # Intialize output folder directories
     if not os.path.isdir(output_dir):
         os.mkdir(output_dir)
 
-    images_dir = os.path.join(output_dir, 'Images')
+    images_dir = os.path.join(output_dir, "Images")
     if not os.path.isdir(images_dir):
         os.mkdir(images_dir)
     for vehicle in vehicle_list:
         if not os.path.isdir(os.path.join(images_dir, vehicle)):
             os.mkdir(os.path.join(images_dir, vehicle))
 
-    matrices_dir = os.path.join(output_dir, 'Matrices')
+    matrices_dir = os.path.join(output_dir, "Matrices")
     if not os.path.isdir(matrices_dir):
         os.mkdir(matrices_dir)
 
+    # Read all csv outputs from detect_vehicles script
     vdfs = []
     for cam_id in cam_ids:
-        vdf = pd.read_csv(f'{cam_id}.csv')
-        vdf.reset_index(names=['v_id'], inplace=True)
-        vdf['cam_id'] = cam_id
+        vdf = pd.read_csv(f"{cam_id}.csv")
+        vdf.reset_index(names=["v_id"], inplace=True)
+        vdf["cam_id"] = cam_id
         vdfs.append(vdf)
 
     avdf = pd.concat(vdfs, ignore_index=True)
 
-    avdf['features'] = avdf['features'].apply(lambda x: torch.tensor(ast.literal_eval(x)).to(device))
-    avdf['features'] = avdf['features'].apply(lambda x: x/x.norm())
+    avdf["features"] = avdf["features"].apply(
+        lambda x: torch.tensor(ast.literal_eval(x)).to(device)
+    ) # set datatype to torch and move to GPU if available 
+    avdf["features"] = avdf["features"].apply(lambda x: x / x.norm()) # Normalise features
 
-    scols = ['v_id', 'v_idt', 'cls_id', 'features', 'Turning Patterns', 'cam_id']
+    # Find cosine similarity between vehicles for each class across cams for each turn/direction
+    scols = ["v_id", "v_idt", "cls_id", "features", "Turning Patterns", "cam_id"]
     dfa_list = []
     for i in range(len(cam_ids)):
-        for j in range(i+1, len(cam_ids)):
-            df1 = avdf.loc[avdf['cam_id'] == cam_ids[i]]
-            df2 = avdf.loc[avdf['cam_id'] == cam_ids[j]]
-            df1_tp = sorted(df1['Turning Patterns'].unique())
-            df2_tp = sorted(df2['Turning Patterns'].unique())
-            pairs = pair_buckets_with_reuse(df1_tp, df2_tp)
+        for j in range(i + 1, len(cam_ids)):
+            df1 = avdf.loc[avdf["cam_id"] == cam_ids[i]]
+            df2 = avdf.loc[avdf["cam_id"] == cam_ids[j]]
+            df1_tp = sorted(df1["Turning Patterns"].unique())
+            df2_tp = sorted(df2["Turning Patterns"].unique())
+            pairs = pair_buckets_with_reuse(df1_tp, df2_tp) # permute cam and turn
             dfs_dict = {}
             for tp_i in range(len(df1_tp)):
                 for tp_j in range(len(df2_tp)):
-                    df1_tp_i = df1.loc[df1['Turning Patterns'] == df1_tp[tp_i]].copy()
-                    df2_tp_j = df2.loc[df2['Turning Patterns'] == df2_tp[tp_j]].copy()
-                    df1_tp_i['v_idt'] = list(range(df1_tp_i.shape[0]))
-                    df2_tp_j['v_idt'] = list(range(df2_tp_j.shape[0]))
-                    dfm = pd.merge(df1_tp_i[scols], df2_tp_j[scols], how='inner', on=['cls_id'], suffixes=['_1', '_2'])
-                    dfm['similarity'] = dfm.apply(lambda x: torch.nn.functional.cosine_similarity(x['features_1'], x['features_2'], dim=0).item(), axis=1).round(2)
-                    dfm.drop(columns=['features_1', 'features_2'], inplace=True)
-                    dfm = dfm.loc[dfm['similarity'] > 0.2].copy()
-                    dfm.sort_values(by=['similarity', 'v_id_1', 'v_id_2'], ascending=[False, True, True], inplace=True)
+                    df1_tp_i = df1.loc[df1["Turning Patterns"] == df1_tp[tp_i]].copy()
+                    df2_tp_j = df2.loc[df2["Turning Patterns"] == df2_tp[tp_j]].copy()
+                    df1_tp_i["v_idt"] = list(range(df1_tp_i.shape[0]))
+                    df2_tp_j["v_idt"] = list(range(df2_tp_j.shape[0]))
+                    dfm = pd.merge(
+                        df1_tp_i[scols],
+                        df2_tp_j[scols],
+                        how="inner",
+                        on=["cls_id"],
+                        suffixes=["_1", "_2"],
+                    )
+                    dfm["similarity"] = dfm.apply(
+                        lambda x: torch.nn.functional.cosine_similarity(
+                            x["features_1"], x["features_2"], dim=0
+                        ).item(),
+                        axis=1,
+                    ).round(2)
+                    dfm.drop(columns=["features_1", "features_2"], inplace=True)
+                    dfm = dfm.loc[dfm["similarity"] > 0.2].copy() # Threshold for minimum similarity
+                    dfm.sort_values(
+                        by=["similarity", "v_id_1", "v_id_2"],
+                        ascending=[False, True, True],
+                        inplace=True,
+                    )
                     v1s = []
                     v2s = []
                     indices = []
-                    for idx,row in dfm.iterrows():
-                        v1 = row['v_id_1']
-                        v2 = row['v_id_2']
-                        v1t = row['v_idt_1']
-                        v2t = row['v_idt_2']
-                        if v1 not in v1s and v2 not in v2s and abs(v1t-v2t) < 30:
+                    for idx, row in dfm.iterrows():
+                        v1 = row["v_id_1"]
+                        v2 = row["v_id_2"]
+                        v1t = row["v_idt_1"]
+                        v2t = row["v_idt_2"]
+                        if v1 not in v1s and v2 not in v2s and abs(v1t - v2t) < 30: # Max distance between vehicles being matched
                             v1s.append(v1)
                             v2s.append(v2)
                             indices.append(idx)
                     dfm = dfm.loc[indices].copy()
-                    dfm.sort_values(by=['v_id_1', 'v_id_2'], ascending=[True,True], inplace=True)
+                    dfm.sort_values(
+                        by=["v_id_1", "v_id_2"], ascending=[True, True], inplace=True
+                    )
                     dfm.reset_index(drop=True, inplace=True)
                     dfs_dict[(df1_tp[tp_i], df2_tp[tp_j])] = dfm
+            # Find a match of cam and turn with max matches
             max_match = 0
             max_pair = None
             for pair in pairs:
@@ -116,66 +142,99 @@ def reid_vehicles(cam_ids, videos, output_dir = '/app/data/CSVS'):
                 dft = dfs_dict[p]
                 dfs_list.append(dft)
             dfs = pd.concat(dfs_list, ignore_index=True)
-            dfs.sort_values(by=['v_id_1', 'v_id_2'], ascending=[True,True], inplace=True)
+            dfs.sort_values(
+                by=["v_id_1", "v_id_2"], ascending=[True, True], inplace=True
+            )
             dfs.reset_index(drop=True, inplace=True)
             dfa_list.append(dfs)
     dfa = pd.concat(dfa_list, ignore_index=True)
-    dfa.reset_index(names=['v_id'], inplace=True)
+    dfa.reset_index(names=["v_id"], inplace=True)
 
-    avcols = ['v_id', 'cam_id', 'fn', 'vehicle', 'x', 'y', 'w', 'h']
+    # Final dataframes for each cam
+    avcols = ["v_id", "cam_id", "fn", "vehicle", "x", "y", "w", "h"]
     fdf_list = []
     for i in range(len(cam_ids)):
-        for j in range(i+1, len(cam_ids)):
-            dfa1 = dfa.loc[dfa['cam_id_1'] == cam_ids[i]][['v_id', 'v_id_1', 'cam_id_1']]
-            dfa2 = dfa.loc[dfa['cam_id_2'] == cam_ids[j]][['v_id', 'v_id_2', 'cam_id_2']]
-            fdf1 = dfa1.merge(avdf[avcols], how='left', left_on=['v_id_1', 'cam_id_1'], right_on=['v_id', 'cam_id'])
-            fdf1.drop(columns=['v_id_1', 'cam_id_1'], inplace=True)
-            fdf1.rename(columns={'v_id_x': 'v_id', 'v_id_y': 'v_id_orig'}, inplace=True)
-            fdf1.sort_values(by='fn', ascending=True, inplace=True)
+        for j in range(i + 1, len(cam_ids)):
+            dfa1 = dfa.loc[dfa["cam_id_1"] == cam_ids[i]][
+                ["v_id", "v_id_1", "cam_id_1"]
+            ]
+            dfa2 = dfa.loc[dfa["cam_id_2"] == cam_ids[j]][
+                ["v_id", "v_id_2", "cam_id_2"]
+            ]
+            fdf1 = dfa1.merge(
+                avdf[avcols],
+                how="left",
+                left_on=["v_id_1", "cam_id_1"],
+                right_on=["v_id", "cam_id"],
+            )
+            fdf1.drop(columns=["v_id_1", "cam_id_1"], inplace=True)
+            fdf1.rename(columns={"v_id_x": "v_id", "v_id_y": "v_id_orig"}, inplace=True)
+            fdf1.sort_values(by="fn", ascending=True, inplace=True)
             fdf1.reset_index(drop=True, inplace=True)
-            fdf2 = dfa2.merge(avdf[avcols], how='left', left_on=['v_id_2', 'cam_id_2'], right_on=['v_id', 'cam_id'])
-            fdf2.drop(columns=['v_id_2', 'cam_id_2'], inplace=True)
-            fdf2.rename(columns={'v_id_x': 'v_id', 'v_id_y': 'v_id_orig'}, inplace=True)
-            fdf2.sort_values(by='fn', ascending=True, inplace=True)
+            fdf2 = dfa2.merge(
+                avdf[avcols],
+                how="left",
+                left_on=["v_id_2", "cam_id_2"],
+                right_on=["v_id", "cam_id"],
+            )
+            fdf2.drop(columns=["v_id_2", "cam_id_2"], inplace=True)
+            fdf2.rename(columns={"v_id_x": "v_id", "v_id_y": "v_id_orig"}, inplace=True)
+            fdf2.sort_values(by="fn", ascending=True, inplace=True)
             fdf2.reset_index(drop=True, inplace=True)
             fdf_list.append(fdf1)
             fdf_list.append(fdf2)
     fdf = pd.concat(fdf_list, ignore_index=True)
 
+    # Annote images of matched vehicles
     for idx, cam_id in enumerate(cam_ids):
         video_cap = cv2.VideoCapture(videos[idx])
-        fdfc = fdf.loc[fdf['cam_id'] == cam_id]
-        for fn in sorted(fdfc['fn'].unique()):
-            fdfcf = fdfc.loc[fdfc['fn'] == fn]
+        fdfc = fdf.loc[fdf["cam_id"] == cam_id]
+        for fn in sorted(fdfc["fn"].unique()):
+            fdfcf = fdfc.loc[fdfc["fn"] == fn]
             for ind, row in fdfcf.iterrows():
-                v_id = row['v_id']
-                vehicle = row['vehicle']
-                x1 = row['x'] - row['w']//2
-                y1 = row['y'] - row['h']//2
-                x2 = row['x'] + row['w']//2
-                y2 = row['y'] + row['h']//2
+                v_id = row["v_id"]
+                vehicle = row["vehicle"]
+                x1 = row["x"] - row["w"] // 2
+                y1 = row["y"] - row["h"] // 2
+                x2 = row["x"] + row["w"] // 2
+                y2 = row["y"] + row["h"] // 2
 
                 video_cap.set(cv2.CAP_PROP_POS_FRAMES, fn)
                 ret, frame = video_cap.read()
                 if not ret:
                     continue
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 5)
-                text = f'{vehicle}_{cam_id}_{fn}_{v_id}'
-                (text_width, text_height), baseline = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
+                text = f"{vehicle}_{cam_id}_{fn}_{v_id}"
+                (text_width, text_height), baseline = cv2.getTextSize(
+                    text, cv2.FONT_HERSHEY_SIMPLEX, 1, 2
+                )
                 box_coords = ((x1, y1 - text_height - 10), (x1 + text_width, y1))
-                cv2.rectangle(frame, box_coords[0], box_coords[1], (0, 0, 255), cv2.FILLED)
-                cv2.putText(frame, text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-                output_file = os.path.join(images_dir, vehicle, f'{text}.jpg')
+                cv2.rectangle(
+                    frame, box_coords[0], box_coords[1], (0, 0, 255), cv2.FILLED
+                )
+                cv2.putText(
+                    frame,
+                    text,
+                    (x1, y1 - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (255, 255, 255),
+                    2,
+                )
+                output_file = os.path.join(images_dir, vehicle, f"{text}.jpg")
                 cv2.imwrite(output_file, frame)
 
+    # Make matrix of re-identified vehicles
     rdf_dict = {}
     matrix_dict = {}
     for vehicle in vehicle_list:
-        fdfv = fdf.loc[fdf['vehicle'] == vehicle]
+        fdfv = fdf.loc[fdf["vehicle"] == vehicle]
         if fdfv.shape[0] == 0:
-            matrix_dict[vehicle] = np.zeros((len(cam_ids), len(cam_ids))).astype(int).tolist()
+            matrix_dict[vehicle] = (
+                np.zeros((len(cam_ids), len(cam_ids))).astype(int).tolist()
+            )
         else:
-            fdfvp = fdfv.pivot_table(index='v_id', columns='cam_id', values='fn')
+            fdfvp = fdfv.pivot_table(index="v_id", columns="cam_id", values="fn")
             rdf_dict[vehicle] = {}
             for i in range(len(cam_ids)):
                 for j in range(len(cam_ids)):
@@ -186,17 +245,17 @@ def reid_vehicles(cam_ids, videos, output_dir = '/app/data/CSVS'):
                     if cam_id_1 not in fdfvp.columns or cam_id_2 not in fdfvp.columns:
                         continue
                     trdf = fdfvp[[cam_id_1, cam_id_2]].dropna()
-                    first = trdf.loc[trdf[cam_id_1]==trdf.min(axis=1)].shape[0]
-                    rdf_dict[vehicle][f'{cam_id_1},{cam_id_2}'] = first
+                    first = trdf.loc[trdf[cam_id_1] == trdf.min(axis=1)].shape[0]
+                    rdf_dict[vehicle][f"{cam_id_1},{cam_id_2}"] = first
             matrix = np.zeros((len(cam_ids), len(cam_ids)))
             reid_value = 0
             for idx1, cam_id_1 in enumerate(cam_ids):
                 for idx2, cam_id_2 in enumerate(cam_ids):
-                    key = f'{cam_id_1},{cam_id_2}'
+                    key = f"{cam_id_1},{cam_id_2}"
                     if key in rdf_dict[vehicle]:
                         reid_value = rdf_dict[vehicle][key]
                         matrix[idx1, idx2] = reid_value
             matrix_dict[vehicle] = matrix.astype(int).tolist()
-        output_file = os.path.join(matrices_dir, f'{vehicle}.json')
-        with open(output_file, 'w') as f:
+        output_file = os.path.join(matrices_dir, f"{vehicle}.json")
+        with open(output_file, "w") as f:
             json.dump(matrix_dict[vehicle], f)
